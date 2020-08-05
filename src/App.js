8d95232b922a3730c61ae573fece11316881cedf
CatalinBalan96 <cata2k18@gmail.com>
2020-08-05 18:03:36 +0300

    Adding some code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello, my name is Catalin',
+      newTodo: '',
+      todos: [{
+        title: 'Learn React',
+        done: false
+      }, {
+        title: 'Learn JSX',
+        done: false
+      }]
+    };
+  }
+
+  formSubmitted(e) {
+    e.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({
+      title: this.state.newTodo,
+      done: false
+    })
+    this.setState({
+      newTodo: '',
+      todos
+    });
+  }
+
+  newTodoChanged(e) {
+    this.setState({
+      newTodo: e.target.value
+    });
+  }
+
+  render() {
+    return(
+      <div className="app">   
+        <h4 className="title">{this.state.message}</h4>
+        <form onSubmit={(e) => this.formSubmitted(e)}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input onChange={(e) => this.newTodoChanged(e)} type="text" name="newTodo" id="newTodo" value={this.state.newTodo}/>
+          <button type="submit">Add Todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li key={todo.title}>{todo.title}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
